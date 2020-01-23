3e3e4159cf70d7db6fa72555e621be9fce8aa548
Amit Rai Sharma <amit.sharma@aarcosolutions.co.uk>
2020-01-23 13:32:37 +0000

    supporting tools added

---
+++ _posts/virtualisation/convert-vmware-to-hyper-v.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module "C:\Program Files\Microsoft Virtual Machine Converter\MvmcCmdlet.psd1"
+
+C:\Hyper-V\dsfok\dsfo.exe "Ubuntu-18-04.vmdk" 512 1024 descriptor1.txt
+
+C:\Hyper-V\dsfok\dsfi.exe "Ubuntu-18-04.vmdk" 512 1024 descriptor1.txt
+
+ConvertTo-MvmcVirtualHardDisk -SourceLiteralPath "Ubuntu-18-04.vmdk" -DestinationLiteralPath "..\" -VhdType DynamicHardDisk -VhdFormat Vhd
+
